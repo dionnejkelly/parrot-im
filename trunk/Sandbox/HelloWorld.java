1c17948e5ef7b6acaf5ee356c8d5d012590fbe0d
kevin.fahy <kevin.fahy@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-05-13 22:41:00 +0000

    Test for Chenny

--- trunk/Sandbox/HelloWorld.java
+++ trunk/Sandbox/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld
       System.out.println("Hello, World!");
       
       System.out.println("Testing: If this prints = SUCCESS!!");
+      System.out.println("Hello Chenny!");
       
       // Nice features to have in Eclipse
       
