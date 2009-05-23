7c6f98b897f6d6cd982985dd602f3033f3590ae1
aaron.khsiu <aaron.khsiu@gmail.com>
2009-05-23 19:20:55 +0000

    First upload didn't work

---
+++ macEclipseTest/src/World.java
@@ -0,0 +1,14 @@
+
+public class World {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Me: Hello World!!");
+		System.out.println("World: Hey..!");
+
+	}
+
+}
