12a8fb441641456a538e4e73474d77bc5ced2ffb
shichan.karachu <shichan.karachu@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-06-06 07:07:36 +0000

    now you can login using the guest account~~~

--- Sandbox/GUI-mockup/src/mainwindow/guestAccountFrame.java
+++ Sandbox/GUI-mockup/src/mainwindow/guestAccountFrame.java
@@ -26,6 +26,9 @@ public class guestAccountFrame extends JFrame{
 	ChatClient core;
 	JFrame mainFrame;
 	
+	private JTextField UNFieldGuest;
+	private JPasswordField PwdFieldGuest;
+	
 	public guestAccountFrame(Model model, ChatClient c, JFrame main){
 		this.model = model;
 		core = c;
@@ -40,28 +43,33 @@ public class guestAccountFrame extends JFrame{
 
 		//select server
 		JComboBox server = new JComboBox (model.getServerList());
+		server.setPreferredSize(new Dimension (200,20));
 
 		//username + password
 		JPanel accountInfoPanel = new JPanel();
 		//set username
-		final JTextField UNFieldGuest = new JTextField();
+		JPanel usernamePanel = new JPanel();
+		UNFieldGuest = new JTextField();
 		UNFieldGuest.setPreferredSize(new Dimension(200,20));
-		accountInfoPanel.add(new JLabel("Username:     "));
-		accountInfoPanel.add(UNFieldGuest);
+		usernamePanel.add(new JLabel("Username:     "));
+		usernamePanel.add(UNFieldGuest);
+		accountInfoPanel.add(usernamePanel);
 		//set password
-		final JPasswordField PwdFieldGuest = new JPasswordField();
+		PwdFieldGuest = new JPasswordField();
 		PwdFieldGuest.setPreferredSize(new Dimension(200,20));
 		accountInfoPanel.add(new JLabel("Password:      "));
 		accountInfoPanel.add(PwdFieldGuest);
 
 		//set ok-cancel button
 		JPanel buttonsPanel = new JPanel();
-		buttonsPanel.setBorder(BorderFactory.createEmptyBorder(20, 50, 10, 10));
+		buttonsPanel.setPreferredSize(new Dimension(20, 90));
+		buttonsPanel.setBorder(BorderFactory.createEmptyBorder(15, 40, 15, 40));
 		GridLayout buttonsLayout = new GridLayout(1,2);
 		buttonsLayout.setHgap(5);
 		buttonsPanel.setLayout(buttonsLayout);
 		//OK Button
 		JButton okButton = new JButton ("OK");
+		okButton.setPreferredSize(new Dimension(20, 40));
 		okButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
             	if (UNFieldGuest.getText().length() != 0 && PwdFieldGuest.getPassword().length != 0){
@@ -73,6 +81,7 @@ public class guestAccountFrame extends JFrame{
 		buttonsPanel.add(okButton);
 		//Cancel Button
 		JButton cancelButton = new JButton ("Cancel");
+		cancelButton.setPreferredSize(new Dimension(20, 40));
 		cancelButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
             	setVisible(false);
@@ -84,9 +93,9 @@ public class guestAccountFrame extends JFrame{
 		JPanel GALPanel = new JPanel();
 		GALPanel.setLayout(new GridLayout(3,1));
 		GALPanel.setBorder(BorderFactory.createEmptyBorder(20, 50, 20, 50));
-		GALPanel.add (server);
-		GALPanel.add(accountInfoPanel);
-		GALPanel.add (buttonsPanel);
+		GALPanel.add (server); //server dropdown menu
+		GALPanel.add (accountInfoPanel); //username+password
+		GALPanel.add (buttonsPanel); //ok+cancel buttons
 
 		getContentPane().add(GALPanel);
 		pack();
@@ -97,7 +106,7 @@ public class guestAccountFrame extends JFrame{
 	private void signIn_ActionPerformed(ActionEvent e) {
 		/*THIS IS FOR CHAT CLIENT : modified ChatClient c*/
 		try {
-			core.login(model.getUsername(), model.getPassword());
+			core.login(UNFieldGuest.getText(), password(PwdFieldGuest.getPassword()));
 			mainFrame.setVisible(false);
 			buddylist buddyWin = new buddylist(core);//pops buddylist window
 		} catch (XMPPException e1) {
@@ -107,4 +116,14 @@ public class guestAccountFrame extends JFrame{
 		}	
 	}
 
+	private String password (char [] pass){
+		String str = new String();
+		str="";
+		
+		for (int i = 0; i < pass.length; i++){
+			str += pass[i];
+		}
+		return str;
+		
+	}
 }
