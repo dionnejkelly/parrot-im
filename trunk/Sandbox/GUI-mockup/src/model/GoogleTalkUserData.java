9543e52da7a9b51c7c31348a96b18c6970ca8e42
kevin.fahy <kevin.fahy@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-06-07 07:07:29 +0000

    Created skeleton userdata classes so that ChatWindowData can be made to function. Currently working on making the model control the open chat windows.

---
+++ trunk/Sandbox/GUI-mockup/src/model/GoogleTalkUserData.java
@@ -0,0 +1,9 @@
+package model;
+
+public class GoogleTalkUserData extends UserData {
+    
+	public GoogleTalkUserData(String accountName, String nickname, 
+			                  String status) {
+		super(accountName, nickname, status);
+	}
+}
