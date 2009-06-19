995c5c29010b3d0da17deaef31e6c3ca36ba6adc
kevin.fahy <kevin.fahy@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-06-19 21:49:36 +0000

    (Jihoon's edit) Made buddy list refresh status.

--- trunk/ParrotIM/src/view/buddylist/buddyPanel.java
+++ trunk/ParrotIM/src/view/buddylist/buddyPanel.java
@@ -55,7 +55,7 @@ public class buddyPanel extends JPanel implements Observer {
     Xmpp chatClient;
     Model model;
     UserData selectedFriend;
-    
+
     private ArrayList<UserData> buddies;
     private DatabaseFunctions bannedAccountList;
 
@@ -63,19 +63,17 @@ public class buddyPanel extends JPanel implements Observer {
         model.addObserver(this);
         setLayout(new BorderLayout());
         setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
-        
-        
-		try {
-			bannedAccountList = new DatabaseFunctions();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-        
+
+        try {
+            bannedAccountList = new DatabaseFunctions();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         this.chatClient = c;
         this.model = model;
         this.chat = null;
@@ -157,69 +155,61 @@ public class buddyPanel extends JPanel implements Observer {
 
         return options;
     }
-    
+
     class blockFriendListener extends MouseAdapter {
         public void mousePressed(MouseEvent event) {
-            
-        	try {
-				blockManager blockedUser = new blockManager(chatClient, model, buddies,bannedAccountList);
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 
-          
+            try {
+                blockManager blockedUser = new blockManager(chatClient, model,
+                        buddies, bannedAccountList);
+            } catch (ClassNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
 
         }
     }
-    
-    
+
     class RightClickMenuBlockFriendListener extends MouseAdapter {
         public void mousePressed(MouseEvent event) {
-            System.out.println("Block this user from the buddy list = " + selectedFriend.toString());
-           
-			
-			
-			
-			chatClient.removeFriend(selectedFriend.toString());
-			
-			buddies.remove(selectedFriend);
-           	boxes[0].removeAll();
-           
+            System.out.println("Block this user from the buddy list = "
+                    + selectedFriend.toString());
+
+            chatClient.removeFriend(selectedFriend.toString());
+
+            buddies.remove(selectedFriend);
+            boxes[0].removeAll();
+
             for (int i = 0; i < buddies.size(); i++) {
                 boxes[0].add(FriendItem(buddies.get(i)));
             }
 
             for (int i = 0; i < boxes[0].getComponentCount(); i++) {
                 boxes[0].getComponent(i).addMouseListener(new SelectListener());
-            	//System.out.println("What is contained the box? " + boxes[0].getComponent(i));
+                // System.out.println("What is contained the box? " +
+                // boxes[0].getComponent(i));
             }
-            
+
             friendList.updateUI();
-            
-            
-			
-			
-				//model.getBannedAccountList().add(selectedFriend.toString());
-				bannedAccountList.setBannedUserList(selectedFriend.toString());
-				try {
-					for (int i = 0; i < bannedAccountList.getBannedUserList().size(); i++) {
-						System.out.println("Banned users = " + bannedAccountList.getBannedUserList().get(i));
 
+            // model.getBannedAccountList().add(selectedFriend.toString());
+            bannedAccountList.setBannedUserList(selectedFriend.toString());
+            try {
+                for (int i = 0; i < bannedAccountList.getBannedUserList()
+                        .size(); i++) {
+                    System.out.println("Banned users = "
+                            + bannedAccountList.getBannedUserList().get(i));
 
-					    
-					}
-				} 
-				catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+                }
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
-    
 
     class RightClickMenuRemoveFriendListener extends MouseAdapter {
         public void mousePressed(MouseEvent event) {
@@ -231,36 +221,41 @@ public class buddyPanel extends JPanel implements Observer {
 
     class removeFriendListener extends MouseAdapter {
         public void mousePressed(MouseEvent event) {
-        	
-        	if (selectedFriend != null) {
-        		System.out.println("Remove this user from the button = "
+
+            if (selectedFriend != null) {
+                System.out.println("Remove this user from the button = "
                         + selectedFriend.toString());
 
                 chatClient.removeFriend(selectedFriend.toString());
-                
-                
-                boolean check = model.getCurrentProfile().getAllFriends().remove(selectedFriend);
-                System.out.println("---------------------------------Deleted = " + check);
-                for (int i = 0; i < model.getCurrentProfile().getAllFriends().size(); i++ ) {
-                	System.out.println("Is it really deleted in the database? " + model.getCurrentProfile().getAllFriends().get(i));
+
+                boolean check = model.getCurrentProfile().getAllFriends()
+                        .remove(selectedFriend);
+                System.out
+                        .println("---------------------------------Deleted = "
+                                + check);
+                for (int i = 0; i < model.getCurrentProfile().getAllFriends()
+                        .size(); i++) {
+                    System.out.println("Is it really deleted in the database? "
+                            + model.getCurrentProfile().getAllFriends().get(i));
                 }
-                
+
                 buddies.remove(selectedFriend);
-               	boxes[0].removeAll();
-               
+                boxes[0].removeAll();
+
                 for (int i = 0; i < buddies.size(); i++) {
                     boxes[0].add(FriendItem(buddies.get(i)));
                 }
 
                 for (int i = 0; i < boxes[0].getComponentCount(); i++) {
-                    boxes[0].getComponent(i).addMouseListener(new SelectListener());
-                	//System.out.println("What is contained the box? " + boxes[0].getComponent(i));
+                    boxes[0].getComponent(i).addMouseListener(
+                            new SelectListener());
+                    // System.out.println("What is contained the box? " +
+                    // boxes[0].getComponent(i));
                 }
-                
+
                 friendList.updateUI();
-        		
-        	}
-            
+
+            }
 
         }
     }
@@ -279,19 +274,17 @@ public class buddyPanel extends JPanel implements Observer {
             if (userFriendID != null && !userFriendID.equals("")) {
                 chatClient.addFriend(userFriendID);
                 JOptionPane.showMessageDialog(null, result);
-                
+
                 buddies.add(new GoogleTalkUserData(userFriendID));
 
                 // add friends to the buddy list
-            
+
                 boxes[0].add(FriendItem(buddies.get(buddies.size() - 1)));
 
-                boxes[0].getComponent(buddies.size() - 1).addMouseListener(new SelectListener());
-                
+                boxes[0].getComponent(buddies.size() - 1).addMouseListener(
+                        new SelectListener());
+
                 friendList.updateUI();
-                
-                
-                
 
             }
             System.out.println("User Input = " + userFriendID);
@@ -311,10 +304,12 @@ public class buddyPanel extends JPanel implements Observer {
 
         // prints the usersnickname to the JPanel.
         JLabel friendName;
-        if (chatClient.getUserPresence(user.getAccountName()).contains("offline")) {
+        if (chatClient.getUserPresence(user.getAccountName()).contains(
+                "offline")) {
             friendName = new JLabel(user.getNickname() + " (Offline)");
         } else {
-            friendName = new JLabel(user.getNickname());
+            friendName = new JLabel(user.getNickname() + " ("
+                    + user.getStatus() + ")");
             // JLabel friendStatus = new JLabel(" - \"" + status + "\"");
         }
 
@@ -342,6 +337,18 @@ public class buddyPanel extends JPanel implements Observer {
                 // add code for if multiple windows exist.
             }
 
+        } else if (arg == UpdatedType.BUDDY) {
+            boxes[0].removeAll();
+            buddies = model.getCurrentProfile().getAllFriends();
+
+            for (int i = 0; i < buddies.size(); i++) {
+                boxes[0].add(FriendItem(buddies.get(i)));
+            }
+
+            for (int i = 0; i < boxes[0].getComponentCount(); i++) {
+                boxes[0].getComponent(i).addMouseListener(new SelectListener());
+            }
+            friendList.updateUI();
         }
     }
 
