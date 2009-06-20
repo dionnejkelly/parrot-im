f267f6aa7c3ebc852250a80b980aa3f93fc8625d
littletomato89 <littletomato89@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-06-20 09:36:36 +0000

    Added Scroller to the Chatwindow. Known Issue: Seems like there is still a bug in the Tree Node.

--- ParrotIM/src/view/profileManager/profileManager.java
+++ ParrotIM/src/view/profileManager/profileManager.java
@@ -191,7 +191,7 @@ public class profileManager extends JFrame
 		// accList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
         
 		JScrollPane acctListScroller = new JScrollPane(accList);
-		acctListScroller.setPreferredSize(new Dimension(300, 220));
+		acctListScroller.setPreferredSize(new Dimension(300, 210));
 		acctListScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER); 
 
 
