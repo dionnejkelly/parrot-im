fb11b69c3b5395dbcb544349b54ba93e17761aae
kevin.fahy <kevin.fahy@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-06-24 23:22:01 +0000

    fix images in jar file

--- trunk/ParrotIM/src/view/mainwindow/HeaderPanel.java
+++ trunk/ParrotIM/src/view/mainwindow/HeaderPanel.java
@@ -20,7 +20,7 @@ public class HeaderPanel extends JPanel{
 		//logo-avatar
 		avatarDisplay = new JLabel ();
 		avatarDisplay.setHorizontalAlignment(SwingConstants.CENTER);
-		avatar = new ImageIcon ("images/buddylist/logoBox.png");
+		avatar = new ImageIcon (this.getClass().getResource("/images/buddylist/logoBox.png"));
 	
 		//TODO: set auto scaling + border later
 		avatarDisplay.setIcon(avatar);
