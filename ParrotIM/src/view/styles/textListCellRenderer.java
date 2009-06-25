b46e5660af946cfd24a4e75284634f1213f54df5
shichan.karachu <shichan.karachu@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-06-25 09:19:58 +0000

    Fixed chat log bug. Changed TextEditor to JList. The scrolling of the text is not very satisfying. Might want to improve it. (revert back to TextEditor? But it's more efficient now)

---
+++ ParrotIM/src/view/styles/textListCellRenderer.java
@@ -0,0 +1,43 @@
+/* textListCellRenderer.java
+ * 
+ * Programmed By:
+ *     Vera Lukman
+ *     
+ * Change Log:
+ *     2009-June-25, VL
+ *         Initial write. Created for ChatLog window log viewer.
+ *         
+ * Known Issues:
+ *     none
+ *     
+ * Copyright (C) 2009  Pirate Captains
+ * 
+ * License: GNU General Public License version 2.
+ * Full license can be found in ParrotIM/LICENSE.txt.
+ */
+
+package view.styles;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+public class textListCellRenderer extends JLabel implements ListCellRenderer{
+
+	public textListCellRenderer()
+	{
+		setOpaque( true );
+	}
+ 
+	public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus)
+	{
+		setText(value.toString());
+		setBackground(Color.WHITE);
+		setForeground(Color.BLACK);
+		return this;
+	}
+
+}
