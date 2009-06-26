c8b2edb4548307830d233bafd993c7130f186038
littletomato89 <littletomato89@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-06-26 01:56:57 +0000

    Updated documentation.

--- ParrotIM/src/API/Buddy.java
+++ ParrotIM/src/API/Buddy.java
@@ -1,31 +1,91 @@
 package API;
 
+/**
+ * Provides Buddy data type that allows users to set their buddy's name and the channel. 
+ */
+
 public class Buddy{
+	
+	// Section
+    // I - Non-Static Data Member
+
+    /**
+     * Holds the user name.
+     */
+	
 	private String username;
+	
+	 /**
+     * Holds the user's channel:  twitter, icq, msn.
+     */
+	
 	private String channel;
-	//channel : twitter, icq, msn
+	
+	// Section
+    // II - Constructors
+
+    /**
+     * Buddy(String username, String channel) allows users to create their buddy. 
+     * 
+     * @param username
+     * @param channel
+     */
+	
 	public Buddy(String username, String channel) {
 		this.username = username;
 		this.channel = channel;
 	}
 	
-	public void setUsername(String u)
+	/**
+     * Sets user's name
+     * 
+     * @param username
+     */
+	
+	public void setUsername(String username)
 	{
-		this.username = u;
+		this.username = username;
 	}
+	
+	/**
+     * Returns user's name
+     * 
+     * @return String
+     */
+	
 	public String getUsername()
 	{
 		return this.username;
 	}
-	public void setChannel(String c)
+	
+	/**
+     * Sets user's channel
+     * 
+     * @param channel
+     */
+	
+	public void setChannel(String channel)
 	{
-		this.channel = c;
+		this.channel = channel;
 	}
+	
+	/**
+     * Returns user's channel
+     * 
+     * @return String
+     */
+	
 	public String getChannel()
 	{
 		return this.channel;
 	}
+	
 	@Override
+	/**
+     * Returns user's name
+     * 
+     * @return String
+     */
 	public String toString(){
 		return username;
 		
--- ParrotIM/src/API/BuddyList.java
+++ ParrotIM/src/API/BuddyList.java
@@ -1,24 +1,72 @@
 package API;
 
+/**
+ * Provides an array of Buddy data that allows users to set their buddies in the array. 
+ */
+
 public class BuddyList{
+	
+	// Section
+    // I - Non-Static Data Member
+
+    /**
+     * Holds the array of buddy list.
+     */
+	
 	private Buddy[] buddyList;
+	
+	// Section
+    // II - Constructors
+
+    /**
+     * BuddyList(Buddy[] buddyList) allows users to create their list of buddies. 
+     * 
+     * @param buddyList
+     */
 	
 	public BuddyList(Buddy[] buddyList) {
 		this.buddyList = buddyList;
 	}
 	
-	public void setBuddyList(Buddy[] bl)
+	/**
+     * Sets the buddy list.
+     * 
+     * @param buddyList
+     */
+	
+	public void setBuddyList(Buddy[] buddyList)
 	{
-		this.buddyList = bl;
+		this.buddyList = buddyList;
 	}
+	
+	/**
+     * Returns the buddy list.
+     * 
+     * @return Buddy[]
+     */
+	
 	public Buddy[] getBuddyList()
 	{
 		return this.buddyList;
 	}
-	public void addBuddy(Buddy bl){
+	
+	/**
+     * Adds a buddy into the buddy list.
+     * 
+     * @param buddy
+     */
+	
+	public void addBuddy(Buddy buddy){
 		// I subtract 1
-		this.buddyList[buddyList.length-1] = bl;
+		this.buddyList[buddyList.length-1] = buddy;
 	}
+	
+	/**
+     * Deletes a buddy into the buddy list.
+     * 
+     * @param n
+     */
+	
 	public void delBuddy(int n){
 		this.buddyList[n] = null;
 	}
