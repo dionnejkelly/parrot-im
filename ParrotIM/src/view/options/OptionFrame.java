4f30b7ee6af5ae9a4097611aaff9778cc10fa252
shichan.karachu <shichan.karachu@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-06-30 08:46:36 +0000

    changed pmLabel a bit. still have a little issue

--- ParrotIM/src/view/options/OptionFrame.java
+++ ParrotIM/src/view/options/OptionFrame.java
@@ -22,26 +22,51 @@
 package view.options;
 
 import java.awt.Dimension;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.sql.SQLException;
 
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 import model.Model;
 
 import controller.MainController;
 
 public class OptionFrame extends JFrame {
+	private JTextArea personalMessage;
+	private MainController controller;
+	
 	public OptionFrame(MainController c, Model model) throws ClassNotFoundException, SQLException{
 		this.setTitle("User Preferences");
+		this.addWindowListener(new OptionWindowListener());
 		
-		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		 
-        getContentPane().add(new OptionPanel(c, model));
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		controller = c;
+		OptionPanel mainPanel =  new OptionPanel(c, model);
+		personalMessage = mainPanel.personalMessage;
+        getContentPane().add(mainPanel);
         setPreferredSize(new Dimension(500,300));
         pack();
    
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
+	}
+	
+	private class OptionWindowListener implements WindowListener{
+
+		public void windowActivated(WindowEvent e) {}
+
+		public void windowClosed(WindowEvent e) {
+			controller.setStatus(personalMessage.getText());
+		}
+
+		public void windowClosing(WindowEvent e) {}
+		public void windowDeactivated(WindowEvent e) {}
+		public void windowDeiconified(WindowEvent e) {}
+		public void windowIconified(WindowEvent e) {}
+		public void windowOpened(WindowEvent e) {}
+		
 	}
 }
