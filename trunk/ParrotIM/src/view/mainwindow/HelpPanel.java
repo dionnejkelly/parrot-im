952b0643870a9b6cca780f3cde5b93d2c566bc8d
littletomato89 <littletomato89@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-06-30 05:51:55 +0000

    Temporarily opens a default web browser in Windows. For Mac or Linux users, we need to the right directory path (More experimentation is needed).

--- trunk/ParrotIM/src/view/mainwindow/HelpPanel.java
+++ trunk/ParrotIM/src/view/mainwindow/HelpPanel.java
@@ -48,61 +48,72 @@ public class HelpPanel extends JPanel{
 	/** SignInPanel constructor. It sets up the panel. */
 	public HelpPanel(String url) {
 		
+		 String browserPath = "C:/Program Files/Internet Explorer/IEXPLORE.EXE"; //Use your browser path 
+       
+         try { 
+        	 
+             String[] b = {browserPath, url}; 
+             Runtime.getRuntime().exec(b); 
+         } 
+         catch (Exception exc) { 
+        	 exc.printStackTrace(); 
+          } 
+		
 //		setLayout(new BorderLayout());
 //		add (new JLabel("Help here"));
 		
 	
-		JFrame frame = new JFrame("Help Menu");
-		
-		frame.setIconImage(new ImageIcon(getcwd() + "/images/mainwindow/logo.png").getImage());
-		try {
-			JEditorPane editorPane = new JEditorPane(url);
-			editorPane.setEditable(false);
-
-			HyperlinkListener hyperlinkListener = new ActivatedHyperlinkListener(editorPane);
-			editorPane.addHyperlinkListener(hyperlinkListener);
-
-			JScrollPane scrollPane = new JScrollPane(editorPane);
-			
-			frame.add(scrollPane);
-		} catch (IOException e) {
-			System.err.println("Unable to load: " + e);
-		}
-
-		frame.setSize(640, 480);
-		frame.setVisible(true);
-		frame.setForeground(Color.WHITE);
+//		JFrame frame = new JFrame("Help Menu");
+//		
+//		frame.setIconImage(new ImageIcon(getcwd() + "/images/mainwindow/logo.png").getImage());
+//		try {
+//			JEditorPane editorPane = new JEditorPane(url);
+//			editorPane.setEditable(false);
+//
+//			HyperlinkListener hyperlinkListener = new ActivatedHyperlinkListener(editorPane);
+//			editorPane.addHyperlinkListener(hyperlinkListener);
+//
+//			JScrollPane scrollPane = new JScrollPane(editorPane);
+//			
+//			frame.add(scrollPane);
+//		} catch (IOException e) {
+//			System.err.println("Unable to load: " + e);
+//		}
+//
+//		frame.setSize(640, 480);
+//		frame.setVisible(true);
+//		frame.setForeground(Color.WHITE);
 	}
 	
 	
-	public static String getcwd() { 
-	    String cwd = System.getProperty("user.dir"); 
-	    return cwd; 
-	}
+//	public static String getcwd() { 
+//	    String cwd = System.getProperty("user.dir"); 
+//	    return cwd; 
+//	}
 }
 
-	class ActivatedHyperlinkListener implements HyperlinkListener {
-		
-		JEditorPane editorPane;
-
-		public ActivatedHyperlinkListener(JEditorPane editorPane) {
-			this.editorPane = editorPane;
-		}
-
-		public void hyperlinkUpdate(HyperlinkEvent hyperlinkEvent) {
-			HyperlinkEvent.EventType type = hyperlinkEvent.getEventType();
-			final URL url = hyperlinkEvent.getURL();
-			if (type == HyperlinkEvent.EventType.ENTERED) {
-				System.out.println("URL: " + url);
-			} else if (type == HyperlinkEvent.EventType.ACTIVATED) {
-				System.out.println("Activated");
-				Document doc = editorPane.getDocument();
-				try {
-					editorPane.setPage(url);
-				} catch (IOException ioException) {
-					System.out.println("Error following link");
-					editorPane.setDocument(doc);
-				}
-			}
-	}
-}
+//	class ActivatedHyperlinkListener implements HyperlinkListener {
+//		
+//		JEditorPane editorPane;
+//
+//		public ActivatedHyperlinkListener(JEditorPane editorPane) {
+//			this.editorPane = editorPane;
+//		}
+//
+//		public void hyperlinkUpdate(HyperlinkEvent hyperlinkEvent) {
+//			HyperlinkEvent.EventType type = hyperlinkEvent.getEventType();
+//			final URL url = hyperlinkEvent.getURL();
+//			if (type == HyperlinkEvent.EventType.ENTERED) {
+//				System.out.println("URL: " + url);
+//			} else if (type == HyperlinkEvent.EventType.ACTIVATED) {
+//				System.out.println("Activated");
+//				Document doc = editorPane.getDocument();
+//				try {
+//					editorPane.setPage(url);
+//				} catch (IOException ioException) {
+//					System.out.println("Error following link");
+//					editorPane.setDocument(doc);
+//				}
+//			}
+//	}
+//}
