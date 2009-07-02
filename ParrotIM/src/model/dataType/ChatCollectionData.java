573b02769fd63d590b976752becf91a5e1f53b46
kevin.fahy <kevin.fahy@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-07-02 05:25:49 +0000

    Partial, non-functional update.
    Implementing an options container for the profile.
    Also, implementing a chat collections class to hold conversations. This will allow us to manage the display of "chat history" and the like.
    
    Also, thinking of implementing a profileCollection class to manage profiles.

---
+++ ParrotIM/src/model/dataType/ChatCollectionData.java
@@ -0,0 +1,24 @@
+/* ChatCollectionData.java
+ * 
+ * Programmed By:
+ *     Kevin Fahy
+ *     
+ * Change Log:
+ *     2009-July-1, KF
+ *         First write. Manages all conversations, including tracking which
+ *         conversation is currently active.
+ *         
+ * Known Issues:
+ *     none
+ *     
+ * Copyright (C) 2009  Pirate Captains
+ * 
+ * License: GNU General Public License version 2.
+ * Full license can be found in ParrotIM/LICENSE.txt.
+ */
+
+package model.dataType;
+
+public class ChatCollectionData {
+
+}
