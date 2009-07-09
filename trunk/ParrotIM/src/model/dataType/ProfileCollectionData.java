ea9b8b590f68000c0655682a0c6897186952b32c
kevin.fahy <kevin.fahy@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-07-09 07:53:50 +0000

    Transferring to other computer.

---
+++ trunk/ParrotIM/src/model/dataType/ProfileCollectionData.java
@@ -0,0 +1,10 @@
+package model.dataType;
+
+import java.util.ArrayList;
+
+public class ProfileCollectionData {
+
+    private ArrayList<ProfileData> profiles;
+    
+    private ProfileData activeProfile;
+}
