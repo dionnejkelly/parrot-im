86679463003641f44605575dfaadbccfd650b47f
littletomato89 <littletomato89@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-07-20 00:07:57 +0000

    Provided MSNManager class, now it needs to be integrated with the database and GUI.

--- trunk/ParrotIM/src/model/dataType/MSNUserData.java
+++ trunk/ParrotIM/src/model/dataType/MSNUserData.java
@@ -1,5 +1,32 @@
 package model.dataType;
 
-public class MSNUserData {
+import model.enumerations.ServerType;
+import model.enumerations.UserStateType;
+
+public class MSNUserData extends UserData implements MSNPerson{
+	
+	   
+	    public MSNUserData(String userID) {
+	        super(userID);
+	    }
+
+	    /**
+	     * Creates a new user with a userID, nickname, and status.
+	     * 
+	     * @param accountName
+	     * @param nickname
+	     * @param status
+	     */
+	    public MSNUserData(String userID, String nickname, String status,
+	            UserStateType state, boolean blocked) {
+	        super(userID, nickname, status, state, blocked);
+	    }
+
+	    // Section
+	    // III - Accessors and Mutators
+
+	    public ServerType getServer() {
+	        return ServerType.MSN;
+	    }
 
 }
