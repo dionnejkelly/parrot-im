0e4035c8f93a6627cb3bfd575ec9b39a4d2cee88
littletomato89 <littletomato89@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-07-22 09:23:59 +0000

    Updating again my code being revived.

--- ParrotIM/src/view/buddylist/BuddyPanel.java
+++ ParrotIM/src/view/buddylist/BuddyPanel.java
@@ -311,7 +311,7 @@ public class BuddyPanel extends GPanel implements Observer {
             } else if (current.getServer() == ServerType.ICQ) {
                 group = 4;
             } else if (current.getServer() == ServerType.MSN) {
-                group = 5;
+                group = 2;
             }
             
             for (int j = 0; j < buddyArray.size() && !found; j++) {
