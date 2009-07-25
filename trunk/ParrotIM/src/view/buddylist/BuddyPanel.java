d646fe2fa13cb271d45aa9032305ef896581498f
littletomato89 <littletomato89@a5200e18-3a86-11de-9b3e-8f53452658d7>
2009-07-25 20:00:27 +0000

    Handled the correct grouping with MSN status states in the buddylist.

--- trunk/ParrotIM/src/view/buddylist/BuddyPanel.java
+++ trunk/ParrotIM/src/view/buddylist/BuddyPanel.java
@@ -434,11 +434,17 @@ public class BuddyPanel extends GPanel implements Observer {
         if (user.getState() == UserStateType.ONLINE) {
             boxIndex = 0;
         } else if (user.getState() == UserStateType.AWAY
-                || user.getState() == UserStateType.BUSY) {
+                || user.getState() == UserStateType.BUSY
+                || user.getState() == UserStateType.PHONE
+                || user.getState() == UserStateType.BRB
+                || user.getState() == UserStateType.LUNCH) {
             boxIndex = 1;
         } else if (user.getState() == UserStateType.OFFLINE) {
             boxIndex = 2;
-        } else {
+        } 
+             
+        else {
+        	System.out.println("I should be blocked!!!");
             boxIndex = 3;
         }
 
